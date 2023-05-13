c70266a404ba6acce5a73aaba065385b286a90a4
Damilola Babalola <dammydeji.dd@gmail.com>
2023-05-13 21:35:04 +0100

    new change clean up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { ConversationChannelPage } from './pages/ConversationChannelPage';
+import { ConversationPage } from './pages/ConversationPage';
+import { LoginPage } from './pages/LoginPage';
+import { RegisterPage } from './pages/RegisterPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="conversations" element={<ConversationPage />}>
+          <Route path=":id" element={<ConversationChannelPage />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
