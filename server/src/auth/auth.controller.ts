649dcb26caad36bd97029a90dbf3fc5a2bb21c96
Damilola Babalola <dammydeji.dd@gmail.com>
2023-05-13 09:22:44 +0100

    Setup user service and  create user method

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -7,18 +7,23 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
-import { Routes, Services } from '../utils/types';
+import { Routes, Services } from '../utils/constants';
 import { IAuthService } from './auth';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { IUserService } from 'src/users/user';
 
 @Controller(Routes.AUTH)
 export class AuthController {
-  constructor(@Inject(Services.AUTH) private authService: IAuthService) {}
+  constructor(
+    @Inject(Services.AUTH) private authService: IAuthService,
+    @Inject(Services.USERS) private userService: IUserService,
+  ) {}
 
   @Post('register')
   @UsePipes(ValidationPipe)
   registerUser(@Body() createUserDto: CreateUserDto) {
     console.log(createUserDto);
+    this.userService.createUser(createUserDto);
   }
 
   @Post('login')
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { IAuthService } from './auth';
+import { Services } from 'src/utils/constants';
+import { IUserService } from 'src/users/user';
 
 @Injectable()
 export class AuthService implements IAuthService {
+  constructor() {}
   validateUser() {
     return 'Hello';
   }
