30d758bf9a9a1e60366d9b4d1adc8d880460f37c
Damilola Babalola <dammydeji.dd@gmail.com>
2023-05-13 01:44:21 +0100

    implenent regiter endpoint

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,8 +1,38 @@
-import { Controller, Inject } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { Routes, Services } from '../utils/types';
 import { IAuthService } from './auth';
+import { CreateUserDto } from './dtos/createUser.dto';
 
 @Controller(Routes.AUTH)
 export class AuthController {
   constructor(@Inject(Services.AUTH) private authService: IAuthService) {}
+
+  @Post('register')
+  @UsePipes(ValidationPipe)
+  registerUser(@Body() createUserDto: CreateUserDto) {
+    console.log(createUserDto);
+  }
+
+  @Post('login')
+  login() {
+    return 'hello';
+  }
+
+  @Get('satus')
+  status() {
+    return 'heloo';
+  }
+
+  @Get('logout')
+  logout() {
+    return 'hello';
+  }
 }
